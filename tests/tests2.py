0fb04484bf99ece3bb999f2e3b5af20b22e74e69
raulcota-vmg <raul@virtualmaterials.com>
2017-06-18 07:37:43 -0600

    Fix mem leak when streamin

--- tests/tests2.py
+++ tests/tests2.py
@@ -12,12 +12,12 @@ class Test(object):
         
         
     def toDict(self):
-        print (self.__dict__)
+        #print (self.__dict__)
         return self.__dict__
         
         
         
-    
+
 def run():
 
     c = Test()
@@ -61,6 +61,8 @@ def run():
     
     f = open(r'C:\temp\temp_russian.json', 'w')
     for i in range(1):
+        print i
+        #a = ujson.dump(e, indent=1, ensure_ascii=1, flush_to_file=1)
         ujson.dump(e, f, indent=1, ensure_ascii=1, flush_to_file=1)
         
     print ( '== after ==')
@@ -80,9 +82,41 @@ def run():
            
 def run1():
     
-    f = open(r'C:\temp\temp_russian.json', 'r')
+    
+    c = Test()
+    c.toDict()
+    
+    a = [1, 2, 3]
+    s = "other thing. Make this into a fairly long string so I can test that streaming from file actually works. The trick is to make sure that it actually tests breaking this down into pieces and then stitches it together"
+    d = {"A": "value", 5: s, 7:a}
+    b = ("l", 5)
+    d[b] = b
+    d['5'] = c
+    
+    
+    e = {}
+    #e['t'] = a
+    #e[12] = d
+    #e[u'yu'] = c
+    #e['xxx'] = True
+    #e['opt'] = False
+    e['thenone'] = None
+    
+    #e["CMDCompressing"] = u"Сжатие файла %s"
+    #e["CMDImported"] = u'''Закончен импорт %s в %s как %s. Несжатые рабочие файлы в %s. Рабочие файлы:
+#%s. Из версии %s'''
+    
+    keys = e.keys()
+    
+    txt = ujson.dumps(e)
     for i in range(1):
+        print i
+        
+        #retVal = ujson.loads(txt)
+        f = open(r'C:\temp\temp_russian.json', 'r')
         retVal = ujson.load(f, stream_from_file=100)
+        f.close()
+        #print sys.getrefcount(f)
         
     print( retVal)
         
@@ -96,7 +130,7 @@ def run2():
     print( retVal)
     
 if __name__ == '__main__':
-    #run()
+    run()
     
     run1()
     #run2()
